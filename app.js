62970d0a4e6c4d730c4386fedba43211416dccd0
CordobaBenjamin <cordobabenjamin37@gmail.com>
2024-03-11 01:10:18 -0300

    seccion 3 terminada3

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 "use strict";
-const a = 10;
-const msg = "Hello";
-function sayHello(msg) {
-    console.log(msg);
-}
+(() => {
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    let powerForce;
+    (function (powerForce) {
+        powerForce[powerForce["Acuaman"] = 0] = "Acuaman";
+        powerForce[powerForce["batman"] = 1] = "batman";
+        powerForce[powerForce["Flash"] = 5] = "Flash";
+        powerForce[powerForce["superman"] = 100] = "superman";
+    })(powerForce || (powerForce = {}));
+    const fuerzaFlash = powerForce.Flash;
+    const fuerzaSuperman = powerForce.superman;
+    const fuerzaBatman = powerForce.batman;
+    const fuerzaAcuaman = powerForce.Acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
+})();
--- app.ts
+++ app.ts
@@ -1,7 +1,47 @@
-const a:number = 10;
-const msg:string = "Hello"
+(() => {
 
-function sayHello (msg: string) {
-    console.log(msg);
-    
-}
+    // Tipos
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
+  
+    const existe:boolean = false;
+  
+    // Tuplas
+    const parejaHeroes:[string, string] = [batman,superman];
+    const villano:[string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados:string [] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+   
+   enum powerForce {
+    Acuaman = 0,
+    batman = 1,
+    Flash = 5,
+    superman = 100,
+   }
+
+    const fuerzaFlash:number = powerForce.Flash;
+    const fuerzaSuperman:number = powerForce.superman;
+    const fuerzaBatman:number = powerForce.batman;
+    const fuerzaAcuaman:number = powerForce.Acuaman;
+
+    // Retorno de funciones
+    function activar_batiseñal():string{
+      return 'activada';
+    }
+  
+    function pedir_ayuda():any{
+    console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = poder.length;
+    console.log( <number>largoDelPoder );
+  
+  
+  })()
+  
+  
